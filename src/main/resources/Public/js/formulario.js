0d64ee68fe5cfb5ae9499d85f0a7df14591790f3
Ubuntu <ubuntu@ip-172-31-91-71.ec2.internal>
2021-04-15 03:46:24 +0000

    .....

--- src/main/resources/Public/js/formulario.js
+++ src/main/resources/Public/js/formulario.js
@@ -61,8 +61,8 @@ $(document).ready(function (){
     }
 
     function conectar() {
-        webSocket = new WebSocket("wss://" + location.hostname + ":" + location.port + "/wsConnect");
-
+        webSocket = new WebSocket("wss://" + location.hostname + "/wsConnect");
+	console.log(location.hostname + ":" + location.port)
         //indicando los eventos:
         webSocket.onmessage = function(data){recibirInformacionServidor(data);};
         webSocket.onopen  = function(e){ console.log("Conectado - status "+this.readyState); };
